7eb3d756389c3c90c8c9f0e1ed762e92e1bbfde1
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-04-09 13:17:51 +0530

    Backspace String Compare
    Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
    
    Example 1:
    
    Input: S = "ab#c", T = "ad#c"
    Output: true
    Explanation: Both S and T become "ac".
    Example 2:
    
    Input: S = "ab##", T = "c#d#"
    Output: true
    Explanation: Both S and T become "".
    Example 3:
    
    Input: S = "a##c", T = "#a#c"
    Output: true
    Explanation: Both S and T become "c".
    Example 4:
    
    Input: S = "a#c", T = "b"
    Output: false
    Explanation: S becomes "c" while T becomes "b".

---
+++ str_compare.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+        vector<char> str1, str2;
+        for(auto x:S)
+        {
+            if(x!='#')
+                str1.push_back(x);
+            else{
+                if(str1.size()) str1.pop_back();;
+            }
+        }
+        for(auto x:T)
+        {
+            if(x!='#')
+                str2.push_back(x);
+            else{
+                if(str2.size()) str2.pop_back();;
+            }
+        }
+        return (str1==str2);
+    }
+};
