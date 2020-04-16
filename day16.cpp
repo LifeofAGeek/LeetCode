26cab3daec2835c310fd8a268650279be42a56b7
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-04-16 17:16:27 +0530

    Valid Parenthesis String
    Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
    
    Any left parenthesis '(' must have a corresponding right parenthesis ')'.
    Any right parenthesis ')' must have a corresponding left parenthesis '('.
    Left parenthesis '(' must go before the corresponding right parenthesis ')'.
    '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
    An empty string is also valid.

--- day16.cpp
+++ day16.cpp
@@ -1,16 +1,43 @@
+//greedy approach
 class Solution {
 public:
     bool checkValidString(string s) {
     int l = s.length() - 1;
     int openCount = 0, closedCount = 0;
+    if(s[0]==')') return false; //always invalid eg.. ")()"
     for (int i = 0; i <= l; i++)
     {
-        if (s[i] == '*' || s[i] == '(') openCount++;
+        if (s[i] == '*' || s[i] == '(') openCount++; //left to right traversing
         else openCount--;
-        if (s[l - i] == '*' || s[l - i] == ')') closedCount++;
+        if (s[l - i] == '*' || s[l - i] == ')') closedCount++; //right to left traversing
         else closedCount--;
-        if (openCount < 0 || closedCount < 0) return false;
+        if (openCount < 0 || closedCount < 0) return false; // ivalid string 
     }
-    return true;
+    return true; // openCount or closedCount >=0 
     }
 };
+
+//famous two stack approach
+classSolution {
+public:
+bool checkValidString(string s) {
+      stack<int> open,ast;
+      for(int i=0;i<s.length();i++)
+      {
+          if(s[i] == ')')
+          {
+              if(!open.empty()) open.pop();
+              else if(!ast.empty()) ast.pop();
+              else return false;
+          }
+          else if(s[i] == '(')   open.push(i);
+          else ast.push(i);
+      }
+      while(!open.empty() && !ast.empty())
+      {
+         if(open.top() > ast.top()) return false;
+         open.pop();
+          ast.pop();
+      }
+        return open.empty();
+    };
