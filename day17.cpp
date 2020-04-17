b7b5d018ad5349d745ba944971aaf26c819aa7a8
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-04-17 15:27:41 +0530

    Number of Islands
    Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.

--- day17.cpp
+++ day17.cpp
@@ -1,34 +1,41 @@
 //DFS
 class Solution {
 public:
+    void traverseIsland(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || col < 0) {
+            return;
+        }
+
+        if (row >= (int)grid.size() || col >= (int)grid[0].size()) {
+            return;
+        }
+
+        if (grid[row][col] == '0') {
+            return;
+        }
+
+        grid[row][col] = '0';
+
+        traverseIsland(grid, row, col + 1);
+        traverseIsland(grid, row + 1, col);
+        traverseIsland(grid, row - 1, col);
+        traverseIsland(grid, row, col - 1);
+    }
+
     int numIslands(vector<vector<char>>& grid) {
-        int x = grid.size(), y = 0, numIslands = 0;
-        if(x) y = grid[0].size();
-        
-        // iterate through grid and call findIsland when grid[i][j] = '1'
-        for(int i = 0; i < x; i++){
-            for(int j = 0; j < y; j++){
-                if(grid[i][j] == '1'){
-                    findIsland(i, j, grid);
-                    numIslands++;
+        int numIsland = 0;
+
+        for (int row = 0; row < (int)grid.size(); ++row) {
+            for (int col = 0; col < (int)grid[0].size(); ++col) {
+                char cell = grid[row][col];
+
+                if (cell == '1') {
+                    traverseIsland(grid, row, col);
+                    ++numIsland;
                 }
             }
         }
-        return numIslands;
-    }
-private:
-    void findIsland(int a, int b, vector<vector<char>>& grid) {
-        int x = grid.size(); int y = grid[0].size();
-        
-        // if out of bounds or not '1', return
-        if( a < 0 or b < 0 or a >= x or b >= y or grid[a][b] != '1') return;
-        
-        // set the '1' to be '0' so not recounted
-        grid[a][b] = '0';
-        
-        // recurse in all 4 directions
-        int changeX[4] = {0,-1,1,0}, changeY[4] = {-1,0,0,1};
-        for(int i = 0; i < 4; i++)  findIsland(a + changeX[i], b + changeY[i], grid);
-        
+
+        return numIsland;
     }
 };
